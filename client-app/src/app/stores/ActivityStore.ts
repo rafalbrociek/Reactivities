25d2a31f846a25c31b38555cff818c4e7c003c39
rafalbrociek <rbrociek@wp.pl>
2024-01-11 20:24:47 +0100

    End of section 9

--- client-app/src/app/stores/ActivityStore.ts
+++ client-app/src/app/stores/ActivityStore.ts
@@ -22,6 +22,18 @@ export default class ActivityStore {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
     } 
 
+    // tworzymy obiekt złożony z par [data as key, Array(Activity) as value]
+    // danej dacie przyporządkoana jest tablica aktywności mających miejsce w danej dacie
+    get groupedActivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[] })
+        )
+    }
+
     loadActivities = async () => {
         this.setLoadingInitial(true);
         try{
